cff19c3c205f7a27d49ad7094e97da64c45c4d7a
mgoodwinuswt <36903790+mgoodwinuswt@users.noreply.github.com>
2018-04-05 15:43:21 -0400

    cleaning
    cleaned up script

--- Apps/Chrome-Install.ps1
+++ Apps/Chrome-Install.ps1
@@ -1,1 +1,6 @@
-$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+$Path = $env:TEMP
+$Installer = "chrome_installer.exe"
+
+Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Wait
+Remove-Item $Path\$Installer
